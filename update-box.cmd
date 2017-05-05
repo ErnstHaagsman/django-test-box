7587c434716942af4514eda81e2f3bb7e76afb36
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2017-05-05 12:48:04 +0200

    Change update script to properly update base box

--- update-box.cmd
+++ update-box.cmd
@@ -1,3 +1,7 @@
+vagrant box remove djangocon/base
+
 vagrant destroy -f && vagrant up && vagrant package
+
+vagrant box add --name djangocon/base package.box
 
 move /Y package.box c:\DjangoCon\base.box
